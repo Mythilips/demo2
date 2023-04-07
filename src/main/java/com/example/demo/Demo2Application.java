2aee59513c1a016b05e0466def9bc8134aaccb31
mythilipappu <mythilipappu31@gmail.com>
2023-04-06 21:19:41 -0500

    Added files

--- src/main/java/com/example/demo/Demo2Application.java
+++ src/main/java/com/example/demo/Demo2Application.java
@@ -2,9 +2,15 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class Demo2Application {
+
+    @GetMapping("/message")
+    public String getMessage(){
+        return "I will die..!!";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Demo2Application.class, args);
